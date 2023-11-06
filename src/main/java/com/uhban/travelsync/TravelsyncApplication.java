773f8f9ab7266f01e054c9e669235243caf3e60b
Jhsysng <screenshot161@gmail.com>
2023-11-06 17:16:28 +0900

    spring init

---
+++ src/main/java/com/uhban/travelsync/TravelsyncApplication.java
@@ -0,0 +1,13 @@
+package com.uhban.travelsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uhban/travelsync/TravelsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uhban.travelsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
